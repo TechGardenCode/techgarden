28ba0835f53df18d21fc6139ec0eef48d24a36e8
Tech Garden <techgardencode@gmail.com>
2025-04-13 13:33:27 -0400

    feat: baseline changes trying to fix tsconfig

--- ui/sprout/src/app/app.component.ts
+++ ui/sprout/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RootComponent } from '@techgarden/root-lib';
 
 @Component({
   selector: 'app-root',

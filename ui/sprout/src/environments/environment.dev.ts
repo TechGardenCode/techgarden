b253f681b61f5ef5d39052a2beddc7bb7b98d581
Tech Garden <techgardencode@gmail.com>
2025-04-13 19:00:41 -0400

    feat: init posthog

--- ui/sprout/src/environments/environment.dev.ts
+++ ui/sprout/src/environments/environment.dev.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://sprout.api.dev.techgarden.gg',
+  POSTHOG_KEY: 'phc_d4H5kgvElXV2A7sL2PGoPNfR0kUOF5KZDv54GzEdU9B',
+  POSTHOG_HOST: 'https://us.i.posthog.com',
 };
